93662725f87333d742acc380530456674be3a520
Foucsi <julien.foucartreact@gmail.com>
2022-12-27 13:24:35 +0100

    update route signin/signup

---
+++ models/users.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const profilSchema = mongoose.Schema({
+  profil: String,
+});
+
+const userSchema = mongoose.Schema({
+  username: String,
+  password: String,
+  email: String,
+  token: String,
+  profil: [profilSchema],
+});
+
+const User = mongoose.model("users", userSchema);
+module.exports = User;
--- routes/users.js
+++ routes/users.js
@@ -6,10 +6,52 @@ const { checkBody } = require("../modules/checkBody");
 const uid2 = require("uid2");
 const token = uid2(32);
 const bcrypt = require("bcrypt");
+const User = require("../models/users");
 
+/* get all users */
+router.get("/all", (req, res) => {
+  User.find({}).then((data) => {
+    res.json({ result: true, users: data });
+  });
+});
 /* GET users listing. */
-router.get("/", function (req, res) {
-  res.json({ result: true });
+router.post("/signup", function (req, res) {
+  if (!checkBody(req.body, ["username", "password"])) {
+    res.json({ result: false, error: "Missing or empty fields" });
+    return;
+  }
+  User.findOne({ username: req.body.username }).then((data) => {
+    if (data === null) {
+      const hash = bcrypt.hashSync(req.body.password, 10);
+      const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: hash,
+        token: token,
+        profilSchema: [], // profil Monteur ou client
+      });
+      newUser.save().then((data) => {
+        res.json({ result: true, user: data });
+      });
+    } else {
+      res.json({ result: false, error: "your account already exists !" });
+    }
+  });
+});
+
+router.post("/signin", (req, res) => {
+  if (!checkBody(req.body, ["username", "password"])) {
+    res.json({ result: false, error: "Missing or empty fields" });
+    return;
+  }
+  User.findOne({ username: req.body.username }).then((data) => {
+    console.log(req.body);
+    if (data && bcrypt.compareSync(req.body.password, data.password)) {
+      res.json({ result: true, user: data });
+    } else {
+      res.json({ result: false, error: "User not found or wrong password" });
+    }
+  });
 });
 
 module.exports = router;
